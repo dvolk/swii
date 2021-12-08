a95f3ccc4810bce7703eacc137099a0de3e08864
Denis Volk <denis.volk@gmail.com>
2021-12-08 19:50:53 +0000

    initial

---
+++ README.md
@@ -0,0 +1,42 @@
+# swii - silly web ii
+
+swii is a very simple, no javascript web frontend for ii. If you're not familiar with ii, don't worry. It's an irc client for weirdos.
+
+You need ii installed and at least one instance running and connected to a server. swii can use multiple ii's connected to different servers.
+
+## Install and run
+
+    pip3 install argh flask humanize requests tailer
+    python3 main.py --irc_home <dir> --port <port>
+
+Here `dir` is the directory containing your `ii` directories.
+
+For example if you started your ii as `ii -i /home/username/irc` then your irc_home would be `/home/username`
+
+## Public-facing swii
+
+If you want to run swii on a public host, you should configure your web server to use basic authentication.
+
+## Multi-user swii
+
+If you want to run a public multi-user swii with user accounts, you can configure your web server to use different user accounts.
+
+for example
+
+    /chat/username1/*
+    /chat/username2/*
+    ...
+
+where `username1` and `username2` are ii dirs inside the swii irc_home
+
+sigh.
+
+## FAQ
+
+### Why doesn't the page refresh automatically?
+
+We're keeping it simple. If that's what you want then get a browser extension that does it.
+
+### Why can't I scroll back infinitely?
+
+Buzz off.
---
+++ main.py
@@ -0,0 +1,120 @@
+import datetime as dt
+import json
+import pathlib
+import time
+
+import argh
+import flask
+import humanize
+import requests
+import tailer
+
+app = flask.Flask(__name__)
+
+irc_home = None
+
+site_header = """
+<html>
+<head>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+ body, h1, h2, h3, h4, h5, h6 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+#table_id td:nth-child(1)  {white-space: nowrap; color: #999; vertical-align: top;}
+a { margin-right: 5px; text-decoration: none }
+a.current { font-weight: bold; }
+</style>
+<title>{{ network_name }}/{{ channel_name }}</title>
+</head>
+
+<body>
+"""
+
+site_footer = """
+</div>
+</body>
+
+</html>
+"""
+
+view_template = site_header + """
+<div class="w3-container">
+<h1>ii web frontend</h1>
+</div>
+<div class="w3-row">
+<div class="w3-col s2 w3-light-blue w3-padding">
+<h2>Channels</h2>
+<p>
+{% for network_name_, channel_list in channels.items() %}
+<h3>{{ network_name_ }}</h3>
+<p style="padding-left: 8px;">
+{% for channel_name_ in channel_list %}
+{% if network_name == network_name_ and channel_name_ == channel_name %}
+<b>{{ channel_name }}</b><br/>
+{% else %}
+<a href="/chat/{{ irc_dir }}/{{ network_name_ }}/{{ channel_name_|replace("#","$") }}">{{ channel_name_ }}</a><br/>
+{% endif %}
+{% endfor %}
+</p>
+{% endfor %}
+</p>
+</div>
+<div class="w3-col s10 w3-light-gray w3-padding">
+<p>
+<input class="w3-input" type="text" placeholder="Type message or command here"></input>
+</p>
+<table id="table_id">
+{%- for line in lines if line %}
+{% set time_str, line_str = ii_line_datefmt(line) %}
+<tr>
+<td>{{ time_str }}</td>
+<td>{{ line_str }}</td>
+{% endfor %}
+</table>
+</p>
+""" + site_footer
+
+
+def ii_line_datefmt(line):
+    try:
+        words = line.split()
+        t = humanize.naturaldelta(time.time() - int(words[0]))
+        return t, " ".join(words[1:])
+    except:
+        return "123", ""
+
+def get_channels(irc_dir):
+    channels = dict()
+    networks = [x.name for x in (irc_home / irc_dir).glob("*")]
+    for network in networks:
+        channels_ = [x.name for x in (irc_home / irc_dir / network).glob("*") if x.name != "in" and x.name != "out"]
+        channels[network] = channels_
+    return channels
+
+
+@app.route("/chat/<irc_dir>/<network_name>/<channel_name>")
+def chat(irc_dir, network_name, channel_name):
+    channel_name = channel_name.replace("$", "#")
+    channels = get_channels(irc_dir)
+
+    with open(irc_home / irc_dir / network_name / channel_name / "out") as f:
+        lines = tailer.tail(f, 25)
+
+    return flask.render_template_string(view_template,
+                                        irc_dir=irc_dir, network_name=network_name, channel_name=channel_name,
+                                        channels=channels,
+                                        lines=reversed(lines),
+                                        ii_line_datefmt=ii_line_datefmt)
+
+
+
+def go(irc_home_="/home/ubuntu", port=12345, debug=False):
+    global irc_home
+    irc_home = pathlib.Path(irc_home_)
+    app.run(port=port, debug=debug)
+
+if __name__ == "__main__":
+    argh.dispatch_command(go)
