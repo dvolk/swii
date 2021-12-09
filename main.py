e6c480c36846dafc1d9dcd5555664ddf24ba76c7
Denis Volk <denis.volk@gmail.com>
2021-12-09 17:34:25 +0000

    refresh page and nick colors

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 import argh
 import flask
 import humanize
-import requests
 
 import tail
 
@@ -14,18 +13,20 @@ app = flask.Flask(__name__)
 
 irc_home = None
 display_n_messages = None
+reload_speed= None
 
 site_header = """
 <html>
 <head>
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/static/w3.css">
+<link rel="stylesheet" href="/static/w3-colors-2021.css">
 <style>
  body, h1, h2, h3, h4, h5, h6 {
   font-family: Arial, Helvetica, sans-serif;
 }
 #table_id td:nth-child(1)  {white-space: nowrap; color: #999; vertical-align: top;}
+#table_id td:nth-child(2)  {white-space: nowrap; vertical-align: top; text-align:right}
 a { margin-right: 5px; text-decoration: none }
 a.current { font-weight: bold; }
 </style>
@@ -37,17 +38,27 @@ a.current { font-weight: bold; }
 
 site_footer = """
 </div>
+<script>
+{% if reload_page %}
+   window.setInterval(function() {
+      var msgboxtext = document.getElementById("msgtextbox").value;
+      if (!document.hidden && document.getSelection().toString() == "" && msgboxtext == "") {
+         window.location.reload(1);
+      }
+   }, 30000)
+{% endif %}
+</script>
 </body>
 
 </html>
 """
 
 view_template = site_header + """
-<div class="w3-container w3-dark-gray">
+<div class="w3-container">
 <h1>ii web frontend</h1>
 </div>
 <div class="w3-row">
-<div class="w3-col s2 w3-blue-gray w3-padding">
+<div class="w3-col s2 w3-2021-mint w3-padding">
 <h2>Networks</h2>
 <p>
 {% for network_name_, channel_list in channels.items() %}
@@ -66,30 +77,64 @@ view_template = site_header + """
 </div>
 <div class="w3-col s10 w3-light-gray w3-padding">
 <p>
+  <a href="{{url_for('chat', irc_dir=irc_dir, network_name=network_name, channel_name=channel_name, skip=skip+25,start=start+25) }}">
+    Up 25 messages
+  </a>&nbsp;&nbsp;
+  <a href="{{url_for('chat', irc_dir=irc_dir, network_name=network_name, channel_name=channel_name, skip=skip-25, start=start-25) }}">
+    Down 25 messages
+  </a>&nbsp;&nbsp;
+  <a href="{{url_for('chat', irc_dir=irc_dir, network_name=network_name, channel_name=channel_name) }}">
+    Reset
+  </a>&nbsp;&nbsp;
+{% if reload_page %}
+  <a href="{{url_for('chat', irc_dir=irc_dir, network_name=network_name, channel_name=channel_name, reload_page=false) }}">Stop reloading</a>
+{% endif %}
+</p>
+<p>
 <table id="table_id">
-{%- for line in lines if line != ("", "") %}
-{% set time_str, line_str = ii_line_datefmt(line) %}
+{%- for line in lines if line != ("", "", "") %}
+{% set time_str, nick_str, line_str = ii_line_fmt(line) %}
 <tr>
 <td>{{ time_str }}</td>
-<td>{{ line_str }}</td>
+{% if nick_str %}
+<td>{{ color_nickname(nick_str)|safe }}</td>
+{% else %}
+<td></td>
+{% endif %}
+{% if nick_str %}
+<td style="padding-left: 5px;">{{ line_str }}</td>
+{% else %}
+<td style="padding-left: 5px; color: #999">{{ line_str }}</td>
+{% endif %}
 {% endfor %}
 </table>
 </p>
-<p>
 <form method="POST">
-<input class="w3-input" type="text" name="user_msg" placeholder="Type message or command here"></input>
-</form>
+<p>
+<input class="w3-input" id="msgtextbox" type="text" name="user_msg" placeholder="Type message or command here"/>
 </p>
+</form>
 """ + site_footer
 
+colors = ["w3-red", "w3-pink", "w3-purple", "w3-indigo", "w3-light-blue", "w3-cyan", "w3-aqua", "w3-teal", "w3-green", "w3-light-green", "w3-sand", "w3-khaki", "w3-yellow", "w3-amber", "w3-orange", "w3-deep-orange", "w3-blue-gray", "w3-brown", "w3-gray", "w3-dark-gray", "w3-pale-red", "w3-pale-yellow", "w3-pale-green", "w3-pale-blue"]
 
-def ii_line_datefmt(line):
+def color_nickname(nickname):
+    color = colors[abs(hash(nickname)) % len(colors)]
+    html_str = f"""<span style="padding: 1px;" class="w3-round { color }">{ nickname[1:-1] }</span>"""
+    print(color, html_str)
+    return html_str
+
+
+def ii_line_fmt(line):
     try:
         words = line.split()
         t = humanize.naturaldelta(time.time() - int(words[0]))
-        return t, " ".join(words[1:])
+        if words[1][0] == "<": # nickname
+            return t, words[1], " ".join(words[2:])
+        else:
+            return t, "", " ".join(words[1:])
     except:
-        return "", ""
+        return "", "", ""
 
 def get_channels(irc_dir):
     channels = dict()
@@ -117,9 +162,15 @@ def chat_index(irc_dir):
 def chat(irc_dir, network_name, channel_name):
     channel_name = channel_name.replace("$", "#")
     channels = get_channels(irc_dir)
+    start = int(flask.request.args.get("start", "25"))
+    skip = int(flask.request.args.get("skip", "0"))
+
+    reload_page = False
+    if skip == 0 and flask.request.args.get("reload_page") != "False":
+        reload_page = True
 
     with open(irc_home / irc_dir / network_name / channel_name / "out") as f:
-        lines = tail.tail(f, display_n_messages)
+        lines = tail.tail(f, start+skip)[skip:start]
 
     if flask.request.method == "POST":
         user_msg = flask.request.form.get("user_msg")
@@ -132,16 +183,21 @@ def chat(irc_dir, network_name, channel_name):
                                             irc_dir=irc_dir, network_name=network_name, channel_name=channel_name,
                                             channels=channels,
                                             lines=lines,
-                                            ii_line_datefmt=ii_line_datefmt)
+                                            ii_line_fmt=ii_line_fmt,
+                                            color_nickname=color_nickname,
+                                            skip=skip, start=start,
+                                            reload_page=reload_page)
 
 
 
 
-def go(irc_home_="/home/ubuntu", display_n_messages_=25, port=12345, debug=False):
+def go(irc_home_="/home/ubuntu", reload_speed_=30, display_n_messages_=25, port=12345, debug=False):
     global irc_home
     irc_home = pathlib.Path(irc_home_)
     global display_n_messages
     display_n_messages = display_n_messages_
+    global reload_speed
+    reload_speed =  reload_speed_
     app.run(port=port, debug=debug)
 
 if __name__ == "__main__":
